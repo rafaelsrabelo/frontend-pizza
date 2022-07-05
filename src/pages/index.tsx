53e332bd68b8d9ac1b9bbc58604a23f6939f18e8
Rafael <rafaelrabelo1610@gmail.com>
2022-07-05 15:23:50 -0300

    Criando component input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
 import Head from "../../node_modules/next/head"
+import styles from "../../styles/home.module.scss"
+import logo from '../../public/images/logo.svg'
+import Image from "../../node_modules/next/image"
+import { Input, TextArea } from "../components/Input/index"
 
 export default function Home() {
   return (
@@ -7,8 +11,20 @@ export default function Home() {
           <title>title da home</title>
         </Head>
 
-        <div>
-          <h1>Bernardo</h1>
+        <div className={styles.containerCenter}>
+          <Image src={logo} alt="Sujeito Pizza" />
+
+          <div className={styles.login}>
+            <form action="">
+              <Input 
+                placeholder="Digite seu email"
+              />
+              <Input 
+                placeholder="Sua senha"
+              />
+              <TextArea placeholder="Digite aqui oh" />
+            </form>
+          </div>
         </div>
       </>
     )
